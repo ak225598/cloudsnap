0502873bfac704523abcde904b0d1020ba413ec3
Ankit <ankit.iiitbh@gmail.com>
2024-04-08 01:18:09 +0530

    feat: Add login and signup API routes

---
+++ src/app/api/users/login/route.js
@@ -0,0 +1,64 @@
+import User from "@/models/userModel";
+import { connect } from "@/dbconfig/dbconnect";
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(req) {
+  try {
+    const reqBody = await req.json();
+    const { email, password } = reqBody;
+    // console.log(reqBody);
+
+    // Check if user already exists
+    const user = await User.findOne({ email });
+
+    if (!user.isVerified) {
+      return NextResponse.json(
+        { message: "Please Verify your account" },
+        { status: 400 }
+      );
+    }
+
+    if (!user) {
+      return NextResponse.json({ message: "Invalid Email" }, { status: 400 });
+    }
+
+    // Check password
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) {
+      console.log("Incorrect password. Please try again");
+      return NextResponse.json(
+        { message: "Incorrect password" },
+        { status: 400 }
+      );
+    }
+
+    // Create token data
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.JWT_SECRET_KEY, {
+      expiresIn: "24h",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (e) {
+    console.log("Something went wrong", e);
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
---
+++ src/app/api/users/signup/route.js
@@ -0,0 +1,44 @@
+import User from "@/models/userModel";
+import { connect } from "@/dbconfig/dbconnect";
+import { NextResponse } from "next/server";
+import bcryptjs from "bcryptjs";
+
+connect();
+
+export async function POST(req) {
+  try {
+    const reqBody = await req.json();
+    const { username, email, password } = reqBody;
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    // Hash password
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    const savedUser = await newUser.save();
+    console.log("User successfully saved");
+    console.log(savedUser);
+
+    return NextResponse.json({
+      message: "User created successfully",
+      success: true,
+      savedUser,
+    });
+  } catch (e) {
+    return NextResponse.json({ error: e.message }, { status: 500 });
+  }
+}
