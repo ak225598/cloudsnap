ab49e0601e45a044003891e1497d6f99ef7d721a
Ankit <ankit.iiitbh@gmail.com>
2024-07-11 03:02:06 +0530

    style: adjust logo and grid appearance

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,10 +4,10 @@ export default function Background() {
       <img
         src="/logo.png"
         alt="Logo"
-        className="w-[150px] h-auto scale-150 opacity-70 rounded-lg"
+        className="w-[150px] h-auto scale-150 opacity-70 rounded-lg -z-10"
       />
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:6rem_4rem]">
-        <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(circle_900px_at_40%_700px,#f3e8ff,transparent)]"></div>
+      <div className="absolute inset-0 -z-20 h-full w-full bg-white bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] bg-[size:4rem_4rem]">
+        <div className="fixed inset-0 -z-30 bg-[radial-gradient(circle_900px_at_40%_1000px,#f3e8ff,transparent)]"></div>
       </div>
     </div>
   );
