244f9bcb0b77141ee84ef4234b1a364a67a964eb
Ankit kumar <ankit.iiitbh@gmail.com>
2024-07-22 18:45:39 +0530

    feat: add password visibility toggle buttons in changePassword page Implement password length validation Update UI with better styling and layout Add redirection for invalid token Refactor form submission logic

--- src/app/changePassword/page.js
+++ src/app/changePassword/page.js
@@ -4,23 +4,33 @@ import { useState, useEffect } from "react";
 import Background from "@/components/Background";
 import toast, { Toaster } from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 const ChangePasswordPage = () => {
   const router = useRouter();
   const [token, setToken] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const urlToken = window.location.search.split("=")[1];
-    // console.log(urlToken);
+    if (urlToken == null) {
+      router.push("/login");
+    }
     setToken(urlToken || "");
   }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (newPassword.length < 6) {
+      setErrorMessage("Password must be at least 6 characters long");
+      return;
+    }
     if (newPassword !== confirmPassword) {
-      toast.error("Passwords do not match");
+      setErrorMessage("Passwords do not match");
       return;
     }
 
@@ -31,60 +41,109 @@ const ChangePasswordPage = () => {
       };
 
       const res = await axios.post("/api/users/changePassword", body);
-      // console.log(res);
       router.push("/login");
       setTimeout(() => {
         toast.success("Password reset successfully");
       }, 1000);
     } catch (error) {
-      // console.log("Error resetting password:", error);
       toast.error("Password reset failed");
     }
   };
 
+  const handleShowPasswordToggle = () => {
+    setShowPassword(!showPassword);
+  };
+
+  const handleShowConfirmPasswordToggle = () => {
+    setShowConfirmPassword(!showConfirmPassword);
+  };
+
   return (
     <>
+      <style jsx global>{`
+        input::-ms-reveal,
+        input::-ms-clear {
+          display: none;
+        }
+      `}</style>
       <Toaster position="top-right" />
       <Background />
-      <div className="w-80 mx-auto max-w-lg bg-white border-2 border-purple-300 rounded-lg p-8 mt-12">
-        <h1 className="text-3xl text-purple-800 font-bold mb-6 text-center">
+      <div className="w-96 mx-auto mt-12 bg-white border border-gray-200 rounded-lg shadow-lg p-8">
+        <h1 className="text-3xl text-center text-purple-800 font-semibold mb-8">
           Reset Password
         </h1>
         <form onSubmit={handleSubmit}>
-          <div className="mb-5">
-            <label htmlFor="newPassword" className="block text-md font-medium">
+          <div className="mb-6">
+            <label
+              htmlFor="newPassword"
+              className="block text-lg font-medium text-gray-700"
+            >
               New Password:
             </label>
-            <input
-              type="password"
-              id="newPassword"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              required
-              className="w-full px-4 py-2 border border-purple-300 rounded-lg focus:outline-none focus:border-purple-600"
-            />
+            <div className="relative mt-2">
+              <input
+                type={showPassword ? "text" : "password"}
+                value={newPassword}
+                onChange={(e) => {
+                  setNewPassword(e.target.value);
+                  setErrorMessage("");
+                }}
+                className="w-full p-4 pr-12 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500"
+                placeholder="Enter your new password"
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-4 flex items-center"
+                onClick={handleShowPasswordToggle}
+              >
+                <Image
+                  src={showPassword ? "/hide.png" : "/view.png"}
+                  alt={showPassword ? "Hide password" : "Show password"}
+                  width={24}
+                  height={24}
+                />
+              </button>
+            </div>
           </div>
-          <div className="mb-4">
+          <div className="mb-6">
             <label
               htmlFor="confirmPassword"
-              className="block text-sm font-medium text-gray-700"
+              className="block text-lg font-medium text-gray-700"
             >
               Confirm New Password:
             </label>
-            <input
-              type="password"
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              required
-              className="w-full px-4 py-2 border border-purple-300 rounded-lg focus:outline-none focus:border-purple-600"
-            />
+            <div className="relative mt-2">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                  setErrorMessage("");
+                }}
+                className="w-full p-4 pr-12 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 focus:border-purple-500"
+                placeholder="Confirm your new password"
+              />
+              <button
+                type="button"
+                className="absolute inset-y-0 right-0 pr-4 flex items-center"
+                onClick={handleShowConfirmPasswordToggle}
+              >
+                <Image
+                  src={showConfirmPassword ? "/hide.png" : "/view.png"}
+                  alt={showConfirmPassword ? "Hide password" : "Show password"}
+                  width={24}
+                  height={24}
+                />
+              </button>
+            </div>
           </div>
-          {/* {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}  */}
-          <div className="flex justify-center mt-5">
+          {errorMessage && (
+            <p className="text-red-500 mb-6 text-center">{errorMessage}</p>
+          )}
+          <div className="flex justify-center mt-6">
             <button
               type="submit"
-              className="w-28 px-4 py-2 bg-purple-600 hover:bg-purple-800 text-white font-semibold rounded-md transition duration-200"
+              className="w-32 py-3 bg-purple-600 hover:bg-purple-800 text-white font-bold rounded-lg transition duration-300"
             >
               Submit
             </button>
